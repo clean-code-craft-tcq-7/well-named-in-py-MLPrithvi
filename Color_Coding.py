df5500d0eaadf8691bf6c985c5fa9c81941130aa
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-11-25 15:03:39 +0530

    Update Color_Coding.py

--- Color_Coding.py
+++ Color_Coding.py
@@ -1,15 +1,15 @@
-MAJOR_COLORS_LIST = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS_LIST = ["Blue", "Orange", "Green", "Brown", "Slate"]
+MajorColorNames = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MinorColorNames = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
 def generate_major_minor_color(pair_number):
   zero_based_pair_number = pair_number - 1
-  major_color_index = zero_based_pair_number // len(MINOR_COLORS_LIST)
-  if major_color_index >= len(MAJOR_COLORS_LIST):
+  major_color_index = zero_based_pair_number // len(MinorColorNames)
+  if major_color_index >= len(MajorColorNames):
     raise Exception('Major Color index out of range')
-  minor_color_index = zero_based_pair_number % len(MINOR_COLORS_LIST)
-  if minor_color_index >= len(MINOR_COLORS_LIST):
+  minor_color_index = zero_based_pair_number % len(MinorColorNames)
+  if minor_color_index >= len(MinorColorNames):
     raise Exception('Minor Color index out of range')
-  return MAJOR_COLORS_LIST[major_color_index], MINOR_COLORS_LIST[minor_color_index]
+  return MajorColorNames[major_color_index], MinorColorNames[minor_color_index]
 
 def verify_major_minor_color(pair_number,
                             expected_major_color, expected_minor_color):
