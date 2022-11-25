7a053e481bff11fbc34e903adb6d7476aa7e22d1
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-11-25 15:02:34 +0530

    Update Number_Coding.py

--- Number_Coding.py
+++ Number_Coding.py
@@ -1,16 +1,16 @@
-MAJOR_COLORS_LIST = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS_LIST = ["Blue", "Orange", "Green", "Brown", "Slate"]
+MajorColorNames = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MinorColorNames = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
 def generate_pair_number(major_color, minor_color):
   try:
-    major_color_index = MAJOR_COLORS_LIST.index(major_color)
+    major_color_index = MajorColorNames.index(major_color)
   except ValueError:
     raise Exception('Major index out of range')
   try:
-    minor_color_index = MINOR_COLORS_LIST.index(minor_color)
+    minor_color_index = MinorColorNames.index(minor_color)
   except ValueError:
     raise Exception('Minor index out of range')
-  return major_color_index * len(MINOR_COLORS_LIST) + minor_color_index + 1
+  return major_color_index * len(MinorColorNames) + minor_color_index + 1
 
 def verify_pair_number(major_color, minor_color, expected_pair_number):
   pair_number = generate_pair_number(major_color, minor_color)
